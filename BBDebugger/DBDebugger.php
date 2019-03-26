cc1a7aae3db8ab64e34cb2893514f11302d6e958
İsmail Satilmiş <ismaiil_0234@hotmail.com>
2019-03-26 16:32:47 +0300

    test code removed

--- BBDebugger/DBDebugger.php
+++ BBDebugger/DBDebugger.php
@@ -20,14 +20,10 @@ class DBDebugger
 
     public function afterQuery($event, $connection)
     {
-        for($i = 1; $i < 2; $i++)
-        {
 
-            $connection->log["endtime"] = microtime(1);
-            $connection->log["speed"]   = number_format($connection->log["endtime"] - $connection->log["starttime"] , 25 , ".",  "");
-            BBDebugger::$logList['db'][] = $connection->log;
-
-        }
+        $connection->log["endtime"] = microtime(1);
+        $connection->log["speed"]   = number_format($connection->log["endtime"] - $connection->log["starttime"] , 25 , ".",  "");
+        BBDebugger::$logList['db'][] = $connection->log;
         
     }
 
